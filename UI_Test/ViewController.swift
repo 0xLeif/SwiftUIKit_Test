72791c824d2463d40766117d8e0684cf17a443a1
Zach Eriksen <zeriksen@clientresourcesinc.com>
2019-10-30 17:12:39 -0500

    init example

--- UI_Test/ViewController.swift
+++ UI_Test/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import SwiftUIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.embed {
+            View(backgroundColor: .clear)
+                .vstack {
+                    [
+                        VStack {
+                            [
+                                Label("Hello World"),
+                                Label("Testing VStack"),
+                                Button("Hello Tap Me!", titleColor: .systemBlue, backgroundColor: .systemYellow) {
+                                    print("Hello You Tapped me!")
+                                }.layer {
+                                    $0.borderWidth = 3
+                                    $0.borderColor = UIColor.systemBlue.cgColor
+                                    $0.cornerRadius = 6
+                                }.accessibility(label: "Tap this button!")
+                            ]
+                        }
+                        .frame(height: 200),
+                        Spacer(height: 4),
+                        HStack { [Label("Body"), Spacer(), Label("Details")] },
+                        Spacer()
+                    ]
+            }
+            .padding()
+        }
+        
     }
-
-
 }
 
