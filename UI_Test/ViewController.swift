f18d884768843fe81f59712c13f655e78eb4b7e8
zmeriksen <zmeriksen@gmail.com>
2019-10-30 19:52:50 -0500

    Updated to 0.2.1

--- UI_Test/ViewController.swift
+++ UI_Test/ViewController.swift
@@ -11,33 +11,44 @@ import SwiftUIKit
 
 class ViewController: UIViewController {
     
+    var headerView: UIView {
+        VStack {
+            [
+                Label("Hello World"),
+                Label("Testing VStack"),
+                Button("Hello Tap Me!", titleColor: .systemBlue, backgroundColor: .systemYellow) {
+                    print("Hello You Tapped me!")
+                }.layer {
+                    $0.borderWidth = 3
+                    $0.borderColor = UIColor.systemBlue.cgColor
+                    $0.cornerRadius = 6
+                }.accessibility(label: "Tap this button!"),
+                UISwitch().configure {
+                    $0.isOn = true
+                    $0.onTintColor = .systemPurple
+                }
+            ]
+        }
+        .frame(height: 200)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.embed {
-            View(backgroundColor: .clear)
-                .vstack {
+            SafeAreaView {
+                VStack {
                     [
-                        VStack {
-                            [
-                                Label("Hello World"),
-                                Label("Testing VStack"),
-                                Button("Hello Tap Me!", titleColor: .systemBlue, backgroundColor: .systemYellow) {
-                                    print("Hello You Tapped me!")
-                                }.layer {
-                                    $0.borderWidth = 3
-                                    $0.borderColor = UIColor.systemBlue.cgColor
-                                    $0.cornerRadius = 6
-                                }.accessibility(label: "Tap this button!")
-                            ]
-                        }
-                        .frame(height: 200),
+                        View(backgroundColor: .lightGray)
+                            .embed { headerView }
+                            .padding()
+                            .layer { $0.cornerRadius = 16 },
                         Spacer(height: 4),
                         HStack { [Label("Body"), Spacer(), Label("Details")] },
                         Spacer()
                     ]
+                }
             }
-            .padding()
         }
         
     }
