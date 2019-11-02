04faee58253cbeef916d64af579b613ddf72e908
zmeriksen <zmeriksen@gmail.com>
2019-11-02 15:36:25 -0500

    Setup example navigation VC

--- UI_Test/TestViewController.swift
+++ UI_Test/TestViewController.swift
@@ -7,24 +7,24 @@
 //
 
 import UIKit
+import SwiftUIKit
 
 class TestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.embed {
+            VStack {
+                [
+                    Label("Hello World")
+                ]
+            }
+        }
+        .configure {
+            $0.backgroundColor = .white
+        }
+        
+        print("Test")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
